8d4ae24bdbe09622841495d641e8cf602c0a58f0
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2023-01-11 14:12:19 +0000

    added car property access tests

--- car.test.js
+++ car.test.js
@@ -12,4 +12,25 @@ describe("testing car class", () => {
         const actual = new Car("Renault", 1000, "petrol");
         expect(actual).toStrictEqual(expected);
     });
+
+    test("can access car manufacturer", () => {
+        const expected = "Renault";
+        const actual = car.manufacturer;
+        expect(actual).toBe(expected);
+    });
+
+    
+    test("can access car price", () => {
+        const expected = 1000;
+        const actual = car.price;
+        expect(actual).toBe(expected);
+    });
+
+    test("can access car engine type", () => {
+        const expected = "petrol";
+        const actual = car.engineType;
+        expect(actual).toBe(expected);
+    });
+
+
 });
